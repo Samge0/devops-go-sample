aa5aedfab59919df51ec1f2c4aea8e50b3bc0e82
shaochengbao <samge720@163.com>
2022-11-25 19:07:58 +0800

    use gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "github.com/gin-gonic/gin"
 )
 
+func index(c *gin.Context) {
+    c.JSON(200, gin.H{
+        "date": "2022-11-25 18:53:25",
+        "name": "samge",
+        "msg": "Hi guys, this is a devops jenkins test of kebesphere",
+        })
+}
+
+func test(c *gin.Context) {
+    c.JSON(200, gin.H{
+        "code": 200,
+        "data": "test data",
+        "msg": "succeed",
+        })
+}
+
 func main() {
-	fmt.Println("devops-go-sample")
-	time.Sleep(time.Duration(60*60*24) * time.Second)
+    r := gin.Default()
+    r.GET("/", index)
+    api := r.Group("/api")
+    {
+        api.GET("/test", test)
+    }
+    r.Run(":8080")
 }
